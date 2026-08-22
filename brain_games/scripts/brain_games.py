df70223bcc95769315c5164e567552f1b93d495b
Mikita <mikit.sazan@gmail.com>
2026-08-22 23:52:21 +0300

    feat: add user greeting

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -4,3 +4,13 @@ def main():
 
 if __name__ == "__main__":
     main()
+from brain_games.cli import welcome_user
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    welcome_user()
+
+
+if __name__ == "__main__":
+    main()
